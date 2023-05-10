c7b7bef885c67a1cd49aedbc19eae149289c96a6
Vadim Brk <Vadim.Brk@outlook.com>
2023-05-11 00:10:00 +0300

    6. Routine code separation, lazy suspension

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import First from "./components/First";
+import { Suspense } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "./index.scss";
+
+import MainPage from "./pages/Main";
+import AboutPage from "./pages/About";
 
 function App() {
   return (
     <div>
-      <First />
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 render(
-  <div>
+  <BrowserRouter>
     <App />
-  </div>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
