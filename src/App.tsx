b6e860c4f72c8b852c67fedaea238fd50ac45e38
Vadim Brk <Vadim.Brk@outlook.com>
2023-05-11 01:19:15 +0300

    7. Organization of styles. Adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Suspense } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import "./index.scss";
+
+import "./styles/index.scss";
 
 import MainPage from "./pages/Main";
 import AboutPage from "./pages/About";
+import { useTheme } from "./theme/ToggleTheme";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>ZZZ</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
