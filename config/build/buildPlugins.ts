709884f52bc196bf5b075958473beffd2486ce37
Vadim Brk <Vadim.Brk@outlook.com>
2023-05-23 13:45:15 +0200

    16. Webpack hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,22 +1,33 @@
-import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from 'webpack';
+import {DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, WebpackPluginInstance} from "webpack";
 
-import { BuildOptions } from './types/config';
+import {BuildOptions} from "./types/config";
 
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({paths, isDev}: BuildOptions): WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HTMLWebpackPlugin({
-      template: paths.html
+      template: paths.html,
     }),
     new ProgressPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contentnash:8].css',
-      chunkFilename: 'css/[name].[contentnash:8].css',
+      filename: "css/[name].[contentnash:8].css",
+      chunkFilename: "css/[name].[contentnash:8].css",
     }),
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
   ];
+  
+  if (isDev) {
+    return [
+      ...plugins,
+      new ReactRefreshPlugin(),
+      new HotModuleReplacementPlugin(),
+    ];
+  } else {
+    return plugins;
+  }
 }
