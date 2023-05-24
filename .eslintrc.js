3e2fc33864a8cfee9422192f36287a8826bb7b87
Vadim Brk <Vadim.Brk@outlook.com>
2023-05-24 17:47:47 +0200

    19. Stylelint. Plugin for i18next

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:i18next/recommended'
   ],
   overrides: [],
   parserOptions: {
@@ -18,6 +19,7 @@ module.exports = {
     'react/jsx-indent': [2, 2],
     '@typescript-eslint/strict-boolean-expressions': 0,
     'react/react-in-jsx-scope': 0,
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
     // REMOVE THIS RULE
     '@typescript-eslint/no-misused-promises': 0,
     '@typescript-eslint/ban-ts-comment': 0
